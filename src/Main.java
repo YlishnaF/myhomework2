46572cd40c3711418f837d340eae48fcfbfe937d
geetbrains <ylishnaf@yandex.ru>
2020-04-12 13:40:52 +0500

    little changes

--- src/Main.java
+++ src/Main.java
@@ -104,26 +104,26 @@ public class Main {
     }
 
     public static void moveArr(int[]arr7, int n) {
-        int[]arr = new int [arr7.length];
+        int[]arr8 = new int [arr7.length];
         if(n>0) {
             for (int i = 0; i < arr7.length; i++) {
                 if (i + n > arr7.length - 1) {
-                    arr[i + n - arr7.length] = arr7[i];
+                    arr8[i + n - arr7.length] = arr7[i];
                 } else {
-                    arr[i + n] = arr7[i];
+                    arr8[i + n] = arr7[i];
                 }
 
             }
         } if(n<0){
             for(int i = 0; i < arr7.length; i++){
                 if(i+n<0){
-                    arr[i+n+arr7.length] = arr7[i];
+                    arr8[i+n+arr7.length] = arr7[i];
                 } else {
-                    arr[i + n] = arr7[i];
+                    arr8[i + n] = arr7[i];
                 }
             }
         }
-        System.out.println(Arrays.toString(arr));
+        System.out.println(Arrays.toString(arr8));
 
     }
 
